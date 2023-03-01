e4b703b28fe6ce472d73ce96a40a0f3967796bd0
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-03-01 08:31:49 +0100

    Update main.js

--- Primeschool/main.js
+++ Primeschool/main.js
@@ -21,3 +21,38 @@ function showCourses (panelIndex) {
  showCourses(0)
 
  // END of  Academy Script 
+
+
+//Newsflash script PLANNER
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", ()=> { const offset = button.dataset.carousel-button === "next" ? 1 : -1
+
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+
+    
+
+    const activeSlide = slides.querySelector("[data-active]")
+
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+
+    
+
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+      
+
+    slides.children[newIndex].dataset.active = true;
+
+    delete activeSlide.dataset.active;
+
+  })  
+
+})
+
+// End of Newsflash script
